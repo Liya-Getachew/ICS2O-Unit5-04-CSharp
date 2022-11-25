36956dff6e91700686b1b72e65c17f1bc2d47188
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-11-25 19:43:59 +0000

    setup

--- main.cs
+++ main.cs
@@ -1,7 +1,34 @@
+// Created by: liya getachew
+// Created on: Nov 25 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function tells the user if they're eligible for discount
+        string day;
+        int age;
+
+        Console.WriteLine("Insert your the day today and your age to see if you're eligible for discount.");
+        Console.WriteLine();
+
+        Console.WriteLine("Your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        Console.WriteLine("today is: ");
+        day = Console.ReadLine();
+        Console.WriteLine();
+
+        if (day == "Tuesday" || day =="Thursday")  (age > 12 && age < 25)
+        {
+          
+        } 
+
+        Console.WriteLine("\nDone.");
+    }
 }
